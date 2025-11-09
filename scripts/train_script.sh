9c64960ca164f12f608b317bfd0bd22923113577
SuperMusey <rishavde.1729@gmail.com>
2025-11-09 18:58:15 -0500

    scripts

--- scripts/train_script.sh
+++ scripts/train_script.sh
@@ -8,7 +8,7 @@ set -x
 SHADOW_ID=${1:-0}
 echo "Training Shadow Model: ${SHADOW_ID}"
 
-cd /jet/home/der/FoundOfPriv/FoundationOfPrivacy/scripts
+cd /ocean/projects/cis250239p/der/FoundOfPriv/FoundationOfPrivacy/scripts
 
 module load python/3.8.6
 module load cuda/12.6.1
@@ -28,9 +28,9 @@ else
 fi
 
 python ft_llm.py \
-  --data_dir /jet/home/der/FoundOfPriv/FoundationOfPrivacy/18734-17731_Project_Phase2_3/data/shadow_data_canary/shadow_${SHADOW_ID} --train_file "train_finetune.json" \
+  --data_dir /ocean/projects/cis250239p/der/FoundOfPriv/FoundationOfPrivacy/18734-17731_Project_Phase2_3/data/val/shadow_data_canary/shadow_${SHADOW_ID} --train_file "train_finetune.json" \
   -m gpt2 --block_size 512 --epochs 3 --batch_size 8 --gradient_accumulation_steps 1 \
-  --lr 2e-4 --outdir /jet/home/der/FoundOfPriv/FoundationOfPrivacy/18734-17731_Project_Phase2_3/models/shadow_model_canary/shadow_${SHADOW_ID}/gpt2_shadow \
+  --lr 2e-4 --outdir /ocean/projects/cis250239p/der/FoundOfPriv/FoundationOfPrivacy/18734-17731_Project_Phase2_3/models/val/shadow_model_canary/shadow_${SHADOW_ID}/gpt2_shadow \
   --lora --lora_r 32 --lora_alpha 64 --lora_dropout 0.05 --merge_lora
 
 
